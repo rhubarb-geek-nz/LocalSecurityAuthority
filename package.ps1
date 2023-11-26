b13a066ac39654f03e99c2ff4fcb13a80436cc1a
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2023-11-26 20:45:23 +0000

    Remove nuspec file

--- package.ps1
+++ package.ps1
@@ -19,59 +19,37 @@
 #
 
 param(
-	$ModuleName = 'LocalSecurityAuthority',
-	$CompanyName = 'rhubarb-geek-nz'
+	$ProjectName = 'LocalSecurityAuthority',
+	$PublishDir = 'bin/Release/netstandard2.0/publish/'
 )
 
 $ErrorActionPreference = "Stop"
 $ProgressPreference = "SilentlyContinue"
-$BINDIR = "bin/Release/netstandard2.0"
-$RID = [System.Runtime.InteropServices.RuntimeInformation]::RuntimeIdentifier
 
 trap
 {
 	throw $PSItem
 }
 
-$xmlDoc = [System.Xml.XmlDocument](Get-Content "$ModuleName.nuspec")
-
-$Version = $xmlDoc.SelectSingleNode("/package/metadata/version").FirstChild.Value
-$ModuleId = $xmlDoc.SelectSingleNode("/package/metadata/id").FirstChild.Value
-$ProjectUri = $xmlDoc.SelectSingleNode("/package/metadata/projectUrl").FirstChild.Value
-$Description = $xmlDoc.SelectSingleNode("/package/metadata/description").FirstChild.Value
-$Author = $xmlDoc.SelectSingleNode("/package/metadata/authors").FirstChild.Value
-$Copyright = $xmlDoc.SelectSingleNode("/package/metadata/copyright").FirstChild.Value
-
-foreach ($Name in "obj", "bin", "$ModuleId")
+function Get-SingleNodeValue([System.Xml.XmlDocument]$doc,[string]$path)
 {
-	if (Test-Path "$Name")
-	{
-		Remove-Item "$Name" -Force -Recurse
-	} 
+    return $doc.SelectSingleNode($path).FirstChild.Value
 }
 
-dotnet build $ModuleName.csproj --configuration Release
+$xmlDoc = [System.Xml.XmlDocument](Get-Content "$ProjectName.csproj")
 
-If ( $LastExitCode -ne 0 )
-{
-	Exit $LastExitCode
-}
-
-$null = New-Item -Path "$ModuleId" -ItemType Directory
-
-foreach ($Filter in "$ModuleName.*")
-{
-	Get-ChildItem -Path "$BINDIR" -Filter $Filter | Foreach-Object {
-		if ((-not($_.Name.EndsWith('.pdb'))) -and (-not($_.Name.EndsWith('.deps.json'))))
-		{
-			Copy-Item -Path $_.FullName -Destination "$ModuleId"
-		}
-	}
-}
+$ModuleId = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/PackageId'
+$Version = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/Version'
+$ProjectUri = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/PackageProjectUrl'
+$Description = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/Description'
+$Author = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/Authors'
+$Copyright = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/Copyright'
+$AssemblyName = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/AssemblyName'
+$CompanyName = Get-SingleNodeValue $xmlDoc '/Project/PropertyGroup/Company'
 
 @"
 @{
-	RootModule = '$ModuleName.dll'
+	RootModule = '$AssemblyName.dll'
 	ModuleVersion = '$Version'
 	GUID = '8691309f-2c6e-4be1-9bb8-79bbd73afd71'
 	Author = '$Author'
@@ -79,7 +57,13 @@ foreach ($Filter in "$ModuleName.*")
 	Copyright = '$Copyright'
 	Description = '$Description'
 	FunctionsToExport = @()
-	CmdletsToExport = @('Open-LsaPolicy', 'Close-LsaPolicy', 'Get-LsaAccountRights', 'Add-LsaAccountRights', 'Remove-LsaAccountRights')
+	CmdletsToExport = @(
+		'Open-LsaPolicy',
+		'Close-LsaPolicy',
+		'Get-LsaAccountRights',
+		'Add-LsaAccountRights',
+		'Remove-LsaAccountRights'
+	)
 	VariablesToExport = '*'
 	AliasesToExport = @()
 	PrivateData = @{
@@ -88,13 +72,4 @@ foreach ($Filter in "$ModuleName.*")
 		}
 	}
 }
-"@ | Set-Content -Path "$ModuleId/$ModuleId.psd1"
-
-Get-Content "./README.md" | Set-Content -Path "$ModuleId/README.md"
-
-nuget pack "$ModuleName.nuspec"
-
-If ( $LastExitCode -ne 0 )
-{
-	Exit $LastExitCode
-}
+"@ | Set-Content -Path "$PublishDir$ModuleId.psd1"
